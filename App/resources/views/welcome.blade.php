a7d0ab647531ff85f585881dc391343648dcd136
ProgrammerJayT <theanthem8@gmail.com>
2022-09-16 01:39:16 +0200

    Added id to paragraph

--- App/resources/views/welcome.blade.php
+++ App/resources/views/welcome.blade.php
@@ -7,11 +7,12 @@
     <body style="height: 100%;margin-top: 100px;">
         <div
             class="d-flex d-xl-flex flex-column justify-content-center align-items-center align-items-xl-center justify-content-xxl-center">
-            <div class="d-xl-flex flex-column align-items-xl-center"><img src="assets/img/Current%20location-bro.png"
-                    style="width: 300px;"></div>
+            <div class="d-xl-flex flex-column align-items-xl-center">
+                <img src="assets/img/Current%20location-bro.png" style="width: 300px;">
+            </div>
             <div class="d-flex d-xl-flex flex-column align-items-center align-items-xl-center" style="margin-top: 50px;">
-                <p style="width: 100%;text-align: center;font-weight: bold;">Ensure that location permissions are allowed</p>
-                <a class="btn" role="button"
+                <p id="message" style="width: 100%;text-align: center;font-weight: bold;">Ensure that location permissions are allowed</p>
+                <a class="btn" role="button" onclick="getLocation()"
                     style="background: #FFC100;border-style: none;color: var(--bs-gray-100);border-radius: 15px;padding-right: 15px;padding-left: 15px;">Get
                     Started</a>
             </div>
