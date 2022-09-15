071ca9a953b7e5e25c7bf12ef806d239219f5b20
ProgrammerJayT <theanthem8@gmail.com>
2022-09-16 00:46:31 +0200

    Return blade view

--- App/app/Http/Controllers/LocationControl.php
+++ App/app/Http/Controllers/LocationControl.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Stevebauman\Location\Facades\Location;
 
 class LocationControl extends Controller
 {
     //
+    public function location()
+    {
+        return view('location');
+    }
 }
